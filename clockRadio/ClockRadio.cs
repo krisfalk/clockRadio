ec475a09b77303312c848bfb2593585e3c0d146e
krisfalk <krisfalk2007@hotmail.com>
2016-08-09 14:03:40 -0500

    initial upload of clock radio

---
+++ clockRadio/ClockRadio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clockRadio
+{
+    public class clockRadio
+    {
+        public string setTimePrint()
+        {
+            Console.WriteLine("Please enter the time: \r\n Hours:");
+            int Hours = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Minutes:");
+            int Minutes = Convert.ToInt32(Console.ReadLine());
+            return Hours + ":" + Minutes;
+            //Console.WriteLine("Clock time is " + Hours + ":" + Minutes);
+        }
+        public double setStationPrint()
+        {
+            Console.WriteLine("Please enter radio station: (ex.101.5)");
+            double station = Convert.ToDouble(Console.ReadLine());
+            return station;
+            //Console.WriteLine("Your current radio station is " + station);
+        }
+        public int setVolumePrint()
+        {
+            Console.WriteLine("Please enter volume: (1-20)");
+            int volume = Convert.ToInt32(Console.ReadLine());
+            return volume;
+            //Console.WriteLine("Volume is currently set at {0} [0-20]", Volume);
+        }
+    }
+}
---
+++ clockRadio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clockRadio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string time;
+            double radioStation;
+            int volume;
+
+            Console.WriteLine("Welcome to my clock radio!");
+            clockRadio clockRadio = new clockRadio();
+
+            time = clockRadio.setTimePrint();
+            radioStation = clockRadio.setStationPrint();
+            volume = clockRadio.setVolumePrint();
+
+            Console.WriteLine("My clock radio is set at: \r\n The Time is " + time + "\r\n Current radio station is on " + radioStation + "\r\n Volume is set at " + volume);
+            Console.ReadLine();
+
+            //Console.WriteLine("Please enter the time:");
+            //Console.WriteLine("Hour:");
+            //hours = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Minutes:");
+            //minutes = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Please enter the radio station: (ex. 101.5)");
+            //radioStation = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("What volume do you want the radio at: (0-20)");
+            //volume = Convert.ToInt32(Console.ReadLine());
+
+            //Console.ReadLine();
+
+            //Console.WriteLine("My clock radio is set at:");
+
+            //clockRadio clockRadio = new clockRadio();
+            //clockRadio.setTimePrint(hours, minutes);
+            //clockRadio.setStationPrint(radioStation);
+            //clockRadio.setVolumePrint(volume);
+
+            //Console.ReadLine();
+        }
+    }
+}
